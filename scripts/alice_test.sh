5d77a89dd4db57725fdfeebdbaf0e273187c8507
azaroche <zar@alice06.cloud.spbu.ru>
2016-11-08 18:32:26 +0300

    Updsate after 20 Jul

--- scripts/alice_test.sh
+++ scripts/alice_test.sh
@@ -9,19 +9,23 @@ cd ~/scripts
 source ALEIN.env
 #export X509_CERT_DIR=/etc/grid-security/certificates/
 
-wget http://alice22.spbu.ru/distr/zar/$SOURCEFILE -O /tmp/$SOURCEFILE
-wget http://alice22.spbu.ru/distr/zar/$SIZEFILE -O /tmp/$SIZEFILE
+wget http://alice22.spbu.ru/distr/zar/alicelists/$SOURCEFILE -O /tmp/$SOURCEFILE
+wget http://alice22.spbu.ru/distr/zar/alicelists/$SIZEFILE -O /tmp/$SIZEFILE
 
 #echo 2
 
 RPATH=root://muon.grid.kiae.ru//eos/fedcloud/zar/alice
+cat $INFILE
+echo $PATH
 
 for i in {0..2}; do
-	for file in `cat /tmp/$SOURCEFILE`; do 
+	cat /tmp/$SOURCEFILE
+	for file in `/bin/cat /tmp/$SOURCEFILE`; do 
 		echo ${RPATH}$i/$file; 
-	done > $INFILE 
-	cat /tmp/$SIZEFILE > ${FILEOUT}.$i
-	./alicetest.sh $INFILE `hostname -f` >> ${FILEOUT}.$i
+	done > $INFILE; 
+	/bin/cat /tmp/$SIZEFILE > ${FILEOUT}.$i
+	./alicetest.sh $INFILE `/bin/hostname -f` >> ${FILEOUT}.$i
+	xrdcp ${FILEOUT}.$i root://muon.grid.kiae.ru//eos/fedcloud/zar/alice_c.$i.`hostname -f`.`date +%s`
 done
 
 echo $FILEOUT
